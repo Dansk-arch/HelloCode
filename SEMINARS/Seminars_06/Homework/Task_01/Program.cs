b7a15d0a3af5e7e4792248fb8394928b1731f52d
Daniil_Korolev <dan.s.korolev@gmail.com>
2023-07-04 00:10:10 +0300

    Added Homework Seminar 06

---
+++ SEMINARS/Seminars_06/Homework/Task_01/Program.cs
@@ -0,0 +1,76 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+
+*/
+
+int QuantityNumber (string decription)
+{
+    int number;
+    Console.Write($"{decription}:");
+
+    while (true)
+    {
+        string temp = Console.ReadLine();
+        if(int.TryParse(temp, out number))
+        {
+            return number;
+        }
+        else
+        {
+            Console.Write($"This number \"{temp}\" is not correct. Try again =>");
+        }  
+    }
+}
+
+int [] Array (int size,string decription)
+{
+    int [] array = new int [size];
+    int count = size;
+    for( int i=0; count>0; i++)
+    {
+        Console.Write($"{decription} -> {count}: введите число ");
+        int number;
+        while (true)
+        {
+            string temp = Console.ReadLine();
+            if(int.TryParse(temp, out number))
+            {
+                array[i]=number;
+                break;
+            }
+            else
+            {
+                Console.Write($"This number \"{number}\" is not correct. Try again =>");
+            }  
+        }
+        count--;
+    }
+    return array;
+}
+
+string PrintArrayInt (int [] array)
+{
+    return string.Join(", ", array);
+}
+
+int FindPositiveNumbers (int [] array)
+{
+    int count = 0;
+    foreach (int item in array)
+    {
+        if(item>0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+int size = QuantityNumber("Введите количество чисел, которые хотите проанадизировать");
+int [] array = Array(size, "Осталось ввести чисел:");
+Console.WriteLine(PrintArrayInt(array));
+Console.Write(FindPositiveNumbers(array));
---
+++ SEMINARS/Seminars_06/Homework/Task_02/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+
+(double XCoordinate, double YCoordinate) SeacIntersectionLines (double bone, double kone, double btwo, double ktwo)
+{
+    double XCoord = (btwo-bone)/(kone-ktwo);
+    double YCoord = (kone*btwo-ktwo*bone)/(kone-ktwo);
+    return (XCoordinate:XCoord, YCoordinate:YCoord);
+}
+
+double GetNumber (string decription)
+{
+    double number;
+    Console.Write($"{decription} =>");
+
+    while (true)
+    {
+        string temp = Console.ReadLine();
+        if(double.TryParse(temp, out number))
+        {
+            return number;
+        }
+        else
+        {
+            Console.Write($"This number \"{temp}\" is not correct. Try again =>");
+        }  
+    }
+}
+
+double bone = GetNumber("Input b1");
+double kone = GetNumber("Input k1");
+double btwo = GetNumber("Input b2");
+double ktwo = GetNumber("Input k2");
+
+var tuple = SeacIntersectionLines(bone,kone,btwo,ktwo);
+
+
+Console.WriteLine($"({tuple.XCoordinate};{tuple.YCoordinate})");
